8fe0aeb2e56894046ee9a316831b7f6f0cad5a00
Mikaruz <gersonsebas2019@gmail.com>
2023-08-10 18:57:58 -0500

    Descripción agregada

--- lib/infrastucture/mappers/movie_mapper.dart
+++ lib/infrastucture/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastucture/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastucture/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,6 +17,26 @@ class MovieMapper {
       posterPath: (movie.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
           : 'no-poster',
+      releaseDate: movie.releaseDate,
+      title: movie.title,
+      video: movie.video,
+      voteAverage: movie.voteAverage,
+      voteCount: movie.voteCount);
+
+  static Movie movieDetailsToEntity(MovieDetails movie) => Movie(
+      adult: movie.adult,
+      backdropPath: (movie.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movie.backdropPath}'
+          : 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled.png',
+      genreIds: movie.genres.map((e) => e.name).toList(),
+      id: movie.id,
+      originalLanguage: movie.originalLanguage,
+      originalTitle: movie.originalTitle,
+      overview: movie.overview,
+      popularity: movie.popularity,
+      posterPath: (movie.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${movie.posterPath}'
+          : 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled.png',
       releaseDate: movie.releaseDate,
       title: movie.title,
       video: movie.video,
