8e1784164c98c802cabb01506a48909c8eb2488a
Mikaruz <gersonsebas2019@gmail.com>
2023-08-12 23:51:46 -0500

    Actores añadidos

---
+++ lib/presentation/providers/actors/actors_repository_provider.dart
@@ -0,0 +1,7 @@
+import 'package:cinemapedia/infrastucture/datasources/actor_moviedb_datasource.dart';
+import 'package:cinemapedia/infrastucture/repositories/actor_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final actorRepositoryProvider = Provider((ref) {
+  return ActorRepositoryImpl(ActorMovieDbDatasource());
+});
--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,3 +1,5 @@
+export 'package:cinemapedia/domain/repositories/actors_repository,dart';
+export 'package:cinemapedia/presentation/providers/actors/actors_by_movie_provider.dart';
 export 'package:cinemapedia/presentation/providers/movies/movies_slideshow_provider.dart';
 export 'package:cinemapedia/presentation/providers/movies/initial_loading_provider.dart';
 export 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
